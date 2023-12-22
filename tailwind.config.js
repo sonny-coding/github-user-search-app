fe7984ca60f80ddef7943131ac08213606f4e833
Son Dao <udemydaohsn@gmail.com>
2023-12-22 07:42:40 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          "ghost-white": "#f6f8ff",
+          blue: "#0079ff",
+          "teal-blue": "#4b6a9b",
+          charcoal: "#2b3442",
+        },
+        dark: {
+          blue: "#0079ff",
+          "zodiac-blue": "#1e2a47",
+          mirage: "#141d2f",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
